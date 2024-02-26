8dc21fde9c1537a2f8fa3db661fbe67a63e43321
VictoriaTorres0 <TorresVictoriaF@outlook.com>
2024-02-26 14:21:38 -0300

    Se creó un div que hace referencia a la base del contenido del formulario y se dió estilos al mismo

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <div className="container-form">
+        <div className="form-vista--1"></div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
