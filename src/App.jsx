821505550880502a53fcc5b8d291c0258f802c85
VictoriaTorres0 <TorresVictoriaF@outlook.com>
2024-02-26 20:52:38 -0300

    Se importó un nuevo archivo Vistatres.jsx y se le dió estilos con Vistatres.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import "./App.css";
 import Vistauno from "../src/components/Vistauno.jsx";
 import Vistados from "../src/components/Vistados.jsx";
+import Vistatres from "../src/components/Vistatres.jsx";
 
 function App() {
   return (
@@ -13,6 +14,11 @@ function App() {
       <div className="container-form">
         <div className="form-base">
           <Vistados />
+        </div>
+      </div>
+      <div className="container-form">
+        <div className="form-base">
+          <Vistatres />
         </div>
       </div>
     </div>
