9deaa40fb68138dbff42c659b3f7215c64d1c5e6
VictoriaTorres0 <TorresVictoriaF@outlook.com>
2024-02-26 17:32:45 -0300

    Se modificó el nombre de una clase.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import "./App.css";
+import Vistauno from "../src/components/Vistauno.jsx";
 
 function App() {
   return (
     <div className="App">
       <div className="container-form">
-        <div className="form-vista--1"></div>
+        <div className="form-base">
+          <Vistauno />
+        </div>
       </div>
     </div>
   );
